e731aeb5fb8a89f17fe3597e552cce30d20d7e20
Jean Zhang <jean.nmn.zhang@gmail.com>
2020-02-02 08:49:56 -0500

    Final page works with dummy data

--- client/src/pages/Final.js
+++ client/src/pages/Final.js
@@ -26,22 +26,98 @@ const Final = (props) => {
                 </TableRow>
                 </TableHead>
                 <TableBody>
-                {/* {rows.map(row => (
-                    <TableRow key={row.name}>
+                {getGroupSongs().map(row => (
+                    <TableRow key={row.song}>
                     <TableCell component="th" scope="row">
-                        {row.name}
+                        {row.song_id}
                     </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
+                    <TableCell>{row.song}</TableCell>
+                    <TableCell>{row.artist}</TableCell>
+                    <TableCell>{row.year}</TableCell>
+                    <TableCell>{row.languages.map(language => language.language_name).join(", ")}</TableCell>
+                    <TableCell>{row.types.map(type => type.type_name).join(", ")}</TableCell>
                     </TableRow>
-                ))} */}
+                ))}
                 </TableBody>
             </Table>
         </TableContainer>  
         </InnerWrapper>    
     </FinalWrapper>
+}
+
+const getGroupSongs = () => {
+    return [
+        {
+          song_id: 27085,
+          song: "Lights Up",
+          artist: "Harry Styles",
+          year: 2019,
+          explicit: false,
+          languages: [
+            {
+              language_id: 2,
+              language_name: "English",
+              hasLanguage: {
+                song_id: 27085,
+                language_id: 2
+              }
+            },
+            {
+                language_id: 2,
+                language_name: "English",
+                hasLanguage: {
+                  song_id: 27085,
+                  language_id: 2
+                }
+              },
+              {
+                language_id: 2,
+                language_name: "English",
+                hasLanguage: {
+                  song_id: 27085,
+                  language_id: 2
+                }
+              },
+          ],
+          types: [
+            {
+              type_id: 33,
+              type_name: "Alternative",
+              hasType: {
+                song_id: 27085,
+                type_id: 33
+              }
+            }
+          ]
+        },
+        {
+          song_id: 27440,
+          song: "Cinnamon Girl",
+          artist: "Lana Del Rey",
+          year: 2019,
+          explicit: false,
+          languages: [
+            {
+              language_id: 2,
+              language_name: "English",
+              hasLanguage: {
+                song_id: 27440,
+                language_id: 2
+              }
+            }
+          ],
+          types: [
+            {
+              type_id: 33,
+              type_name: "Alternative",
+              hasType: {
+                song_id: 27440,
+                type_id: 33
+              }
+            }
+          ]
+        }
+      ];
 }
 
 const FinalWrapper = styled(Wrapper)``;
