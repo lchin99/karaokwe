2958c5934126a168d2fc7d0885249b15ad0339e7
olivia petrillo <opetrillo92@gmail.com>
2020-02-02 00:14:37 -0500

    added attribute to not make createdAt timestamp columns

--- models/sequelize.js
+++ models/sequelize.js
@@ -2,7 +2,10 @@ const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize('local', 'root', 'Karaoke123', {
   host: 'localhost',
-  dialect: 'mysql'
-})
+  dialect: 'mysql',
+  define: {
+    timestamps: false
+  }
+});
 
 module.exports = sequelize;
