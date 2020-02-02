b067dc522569504bc681de6e09c5215e819f9abe
Jean Zhang <jean.nmn.zhang@gmail.com>
2020-02-02 04:54:13 -0500

    swiping works basics

--- client/src/pages/PickSongs.js
+++ client/src/pages/PickSongs.js
@@ -1,12 +1,112 @@
 import React from 'react';
-import { Button, Card, Wrapper } from "../components/Base/basecomponents";
+import styled from "styled-components";
+import TinderCard from 'react-tinder-card'
+import { Wrapper } from "../components/Base/basecomponents";
 
 const PickSongs = () => {
+    const likedSongs = [];
+    const swiped = (direction, song_id) => {
+        if (direction === "right") {
+            likedSongs.push(song_id);
+        }
+    }
+
     return <Wrapper>
-        <Card>
-            <p>PICK SONGS PAGE</p>
-        </Card>
+        <CardWrapper>
+            <div className='cardContainer'>
+            {getAllSongs().map((song) =>
+                <TinderCard className='swipe' key={song.song_id} onSwipe={(dir) => swiped(dir, song.song_id)}>
+                <TextCard song={song} />
+                </TinderCard>
+            )}
+            </div>
+        </CardWrapper>
     </Wrapper>
 }
+
+const TextCard = ({ song }) => {
+    return <div className='card'>
+        <h2>{song.song}</h2>
+        <h4>Artist: {song.artist} <br /> Released: {song.year} </h4>
+        <h4>Languages: {song.languages.map(language => language.language_name).join(", ")}</h4>
+    </div>
+}
+
+const getAllSongs = () => {
+    return [
+        {
+          song_id: 27085,
+          song: "Lights Up",
+          artist: "Harry Styles",
+          year: 2019,
+          explicit: false,
+          languages: [
+            {
+              language_id: 2,
+              language_name: "English",
+              hasLanguage: {
+                song_id: 27085,
+                language_id: 2
+              }
+            },
+            {
+                language_id: 2,
+                language_name: "English",
+                hasLanguage: {
+                  song_id: 27085,
+                  language_id: 2
+                }
+              },
+              {
+                language_id: 2,
+                language_name: "English",
+                hasLanguage: {
+                  song_id: 27085,
+                  language_id: 2
+                }
+              },
+          ],
+          types: [
+            {
+              type_id: 33,
+              type_name: "Alternative",
+              hasType: {
+                song_id: 27085,
+                type_id: 33
+              }
+            }
+          ]
+        },
+        {
+          song_id: 27440,
+          song: "Cinnamon Girl",
+          artist: "Lana Del Rey",
+          year: 2019,
+          explicit: false,
+          languages: [
+            {
+              language_id: 2,
+              language_name: "English",
+              hasLanguage: {
+                song_id: 27440,
+                language_id: 2
+              }
+            }
+          ],
+          types: [
+            {
+              type_id: 33,
+              type_name: "Alternative",
+              hasType: {
+                song_id: 27440,
+                type_id: 33
+              }
+            }
+          ]
+        }
+      ];
+}
+
+const CardWrapper = styled.div``;
 
 export default PickSongs
